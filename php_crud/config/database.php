e35be2503146eee9e2e78b003fb9fd67a1b4fa93
Miranda Gaudet <Gaudetmg@clarkson.edu>
2021-02-15 13:06:03 -0500

    added password

--- php_crud/config/database.php
+++ php_crud/config/database.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db_name = "php_crud";
 $username = "root";
-$password = "";
+$password = "Mirandagracegaudet123$";
 
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
