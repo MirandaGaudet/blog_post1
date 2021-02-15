a9c98cce16ba0afc3064d35dce6512fda57213f6
Miranda Gaudet <Gaudetmg@clarkson.edu>
2021-02-15 13:15:11 -0500

    changed bootstrap

--- php_crud/create.php
+++ php_crud/create.php
@@ -4,7 +4,7 @@
     <title>Create a record</title>
 
     <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/5.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
 </head>
 <body>
@@ -91,7 +91,7 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 <!-- Latest compiled and minified Bootstrap JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
 </html>
